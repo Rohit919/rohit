e4a92e40a8ffc06ae4e19ad334c08f0eaf55a2ae
Rohit Kumar <Rohitkumar919@users.noreply.github.com>
2020-12-02 23:11:02 +0530

    Updates

---
+++ precache-manifest.25b20f8aab6e5e51cf48828c867f992a.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b117c41672c0de85ff25120ab1f6ef1",
+    "url": "/rohit/index.html"
+  },
+  {
+    "revision": "15faed226e369b874d5e",
+    "url": "/rohit/static/css/main.b9a1ccad.chunk.css"
+  },
+  {
+    "revision": "48110004af222aa756da",
+    "url": "/rohit/static/js/2.5a80a986.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rohit/static/js/2.5a80a986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15faed226e369b874d5e",
+    "url": "/rohit/static/js/main.0c2f3122.chunk.js"
+  },
+  {
+    "revision": "a6316ae00e18415b1049",
+    "url": "/rohit/static/js/runtime-main.898c5936.js"
+  },
+  {
+    "revision": "91aac6ed2ff2ab47ce11ca3e3e6cd930",
+    "url": "/rohit/static/media/algo.91aac6ed.svg"
+  },
+  {
+    "revision": "5550a208f74942e2b22037f102a56d31",
+    "url": "/rohit/static/media/api.5550a208.svg"
+  },
+  {
+    "revision": "79f46787e2e8daee9a40eda7a11f0f65",
+    "url": "/rohit/static/media/backend.79f46787.svg"
+  },
+  {
+    "revision": "fb7ad49e77a05107eb4fac307a1c2f62",
+    "url": "/rohit/static/media/c.fb7ad49e.svg"
+  },
+  {
+    "revision": "92e09e5c0d2bfd1e2342a8e54e32e120",
+    "url": "/rohit/static/media/computer.92e09e5c.svg"
+  },
+  {
+    "revision": "e358d69b11521d72495ee93ee990ce81",
+    "url": "/rohit/static/media/covid.e358d69b.png"
+  },
+  {
+    "revision": "76db1b9a84889d843db17bb63d213e5d",
+    "url": "/rohit/static/media/css.76db1b9a.svg"
+  },
+  {
+    "revision": "a9d33836b3312f209bb729b230437099",
+    "url": "/rohit/static/media/cv.a9d33836.svg"
+  },
+  {
+    "revision": "5644467d3baf9901a0b55b9ff0d12d67",
+    "url": "/rohit/static/media/eclipse.5644467d.svg"
+  },
+  {
+    "revision": "70454fa7c11267547b1c96be688dbd81",
+    "url": "/rohit/static/media/edit.70454fa7.svg"
+  },
+  {
+    "revision": "56881d13b8dee11f6e3e3a7853a66d38",
+    "url": "/rohit/static/media/facebook.56881d13.svg"
+  },
+  {
+    "revision": "901663005f1d8268ebaabb2f9fc1ac76",
+    "url": "/rohit/static/media/flickr.90166300.svg"
+  },
+  {
+    "revision": "16e803d8f64e07fce7ec2fd71287a6b8",
+    "url": "/rohit/static/media/github.16e803d8.svg"
+  },
+  {
+    "revision": "524ad5c0084e19bdeafb51a5430dceda",
+    "url": "/rohit/static/media/html.524ad5c0.svg"
+  },
+  {
+    "revision": "3295eb0a826cc4537bf582edd269deba",
+    "url": "/rohit/static/media/instagram.3295eb0a.svg"
+  },
+  {
+    "revision": "05dba3179852a52dbdb5f38fdbb763f1",
+    "url": "/rohit/static/media/java-script.05dba317.svg"
+  },
+  {
+    "revision": "d419db0eeace3783c62476bd1674c57f",
+    "url": "/rohit/static/media/java.d419db0e.svg"
+  },
+  {
+    "revision": "4a8c484be1094c977c6ebd1dc4728b96",
+    "url": "/rohit/static/media/linkedin.4a8c484b.svg"
+  },
+  {
+    "revision": "e33c6e4f07989384c8faf7ec5627ca9b",
+    "url": "/rohit/static/media/medium.e33c6e4f.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/rohit/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "9c3de1804dae07a63d9786186e133075",
+    "url": "/rohit/static/media/officecomputer.9c3de180.png"
+  },
+  {
+    "revision": "dd66d4483c03b57ac5b16ce98d17384c",
+    "url": "/rohit/static/media/pin.dd66d448.svg"
+  },
+  {
+    "revision": "59bce7d7bb0b52fb5fb5a6f266b9ca62",
+    "url": "/rohit/static/media/ppt.59bce7d7.svg"
+  },
+  {
+    "revision": "3b32a5b098e97c20438209415144261e",
+    "url": "/rohit/static/media/puzzle.3b32a5b0.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/rohit/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/rohit/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "ddf988556775e822c5d3d3c7a1678717",
+    "url": "/rohit/static/media/repair.ddf98855.svg"
+  },
+  {
+    "revision": "85f27e359c03b3206eaf9633247503f1",
+    "url": "/rohit/static/media/resume.85f27e35.pdf"
+  },
+  {
+    "revision": "7d0d9f36dfd11500448626522d36fde3",
+    "url": "/rohit/static/media/sql.7d0d9f36.svg"
+  },
+  {
+    "revision": "388b854c0e63f94111a22b46123cc68f",
+    "url": "/rohit/static/media/telegram.388b854c.svg"
+  },
+  {
+    "revision": "b694726b02716c583e41a346ac18c8b1",
+    "url": "/rohit/static/media/twitter.b694726b.svg"
+  },
+  {
+    "revision": "9ee372929dbba323844e1e082db62d90",
+    "url": "/rohit/static/media/ubuntu.9ee37292.svg"
+  },
+  {
+    "revision": "bc56c4b3bab57a5385636da380d8c85d",
+    "url": "/rohit/static/media/windows.bc56c4b3.svg"
+  }
+]);
